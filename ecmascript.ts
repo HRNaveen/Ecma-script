fc3e0c9d9f84c3ec46c45f359641580bdd869c4b
naveen <naveen.hr@exathought.com>
2022-11-25 14:57:01 +0530

    learnt about object literals,Destructuring array,Destructuring objects,String templets,class,class body and methods,class inheritance

--- ecmascript.ts
+++ ecmascript.ts
@@ -130,9 +130,169 @@ let displayColours =function(message,...colors){
     
 }
 let message="List of colors"
+
+let colorsArray=["Orange","Yellow","Indigo"];
+displayColours(message,...colorsArray)
 displayColours(message,"Red");
 displayColours(message,"Red","Blue");
-displayColours(message,"Red","Blue","green")
+displayColours(message,"Red","Blue","green");
+
+let firstName="chandler";
+let lastName="Bings";
+let Person={
+    firstName,
+    lastName
+};
+console.log(Person.firstName);
+console.log(Person.lastName);
+
+function createPerson(firstName,lastName,age){
+    let fullName=firstName+" "+lastName;
+    return {
+        firstName,
+        lastName,
+        fullName,
+        //isSenior:function(){
+        isSenior(){
+            return age>60;
+        }}
+    }
+let p=createPerson("Ross","Geller",66);
+console.log(p.firstName);
+console.log(p.lastName);
+console.log(p.fullName);
+console.log(p.isSenior());
+
+let ln="last name";
+let Person1={
+    "first name":"chandler",
+    [ln]:"Bing"
+}
+console.log(Person1);
+
+//Destructuring array
+
+//let emp=["chandler","Bing","Male"];
+//let[fName, lName, gender]=emp;
+//let[fName, lName]=emp;
+let emp=["chandler","Bing","Male"];
+//let[fName, lName, gender]=emp;
+//let[,, gender]=emp;
+let[fName, ...elements]=emp;
+
+console.log(fName);
+//console.log(lName);
+//console.log(gender);
+console.log(elements);
+
+//destructuring of objects
+
+let emp1={
+    fname:"chandler",
+    lname:"Bing",
+    gender:"Male"
+}
+let{fname,lname,gender}=emp1;
+console.log(fname);
+console.log(lname);
+console.log(gender);
+let{fname:f,lname:l,gender:g}=emp1;
+console.log(f);
+console.log(l);
+console.log(g);
+
+//String Templets
+let user="chandler";
+let greet="Welcome "+user+" to es15";
+console.log(greet);
+
+//for of loop
+let colors=["Red","Blue","Green"];
+for(let index in colors)
+{
+    console.log(colors[index]);
+}
+for(let color of colors){
+    console.log(color);
+}
+let letters="abcdefghijklmnopqrstuvwxyz";
+for(let letter of letters){
+    console.log(letter);
+}
+
+//class
+class Person2{
+    greet(){
+
+    }
+}
+let p1=new Person2();
+console.log(typeof Person2);//function
+console.log(p1.greet===Person2.prototype.greet);
+
+
+//class body and methods
+class Person3{
+    constructor(name){
+        this.name=name;
+        console.log(this.name);
+        
+    }
+    static staticMethod(){
+        console.log("Static method");
+        
+    }
+    greetPerson(){
+        console.log("Hello "+this.name);
+        
+    }
+}
+let p3=new Person3("chandler");
+console.log(Person3.staticMethod());
+console.log(p3.greetPerson());
+
+
+
+class Person4{
+    constructor(name){
+        this.name=name;
+        console.log(this.name+" Parent constructor");
+        
+    }
+    getId(){
+        return 10;
+    }
+}
+class Employee1 extends Person4{
+    constructor(name){
+        super(name)
+        console.log(name+" child constructor");
+        
+    }
+    getId(){
+        return 50;
+    }
+}
+let e= new Employee1("chandler");
+console.log("Id "+e.getId());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
